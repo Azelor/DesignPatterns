31fa3099db81304eed05dcbca5b6eb5ea433e8a9
Kaarel Sööt <kaarel.soot@ignite.ee>
2018-10-19 22:19:16 +0300

    Rename method

--- DeliveryApplication/DeliveryApp.cs
+++ DeliveryApplication/DeliveryApp.cs
@@ -4,10 +4,67 @@ namespace DeliveryApplication
 {
     class DeliveryApp
     {
-        public static void Main(string[] args)
+        public static void Main(string[] args) // main method
         {
-            var deliveryApp = new DeliveryApp();;
+            var deliveryApp = new DeliveryApp();
+            deliveryApp.BeginOperation();
         }
-    }    
-}
 
+        private void BeginOperation()
+        {
+            IDeliveryVehicle deliveryBike = new DeliveryBike();
+            IDeliveryVehicle deliveryCar = new DeliveryCar();
+            IDeliveryVehicle deliveryVan = new DeliveryVan();
+            IDeliveryVehicle deliveryTruck = new DeliveryTruck();
+           
+            Console.WriteLine("Deliveries:");
+           
+            MakeDelivery(deliveryBike);
+            MakeDelivery(deliveryCar);
+            MakeDelivery(deliveryVan);
+            MakeDelivery(deliveryTruck);
+        }
+
+        private void MakeDelivery(IDeliveryVehicle vehicle)
+        {
+            vehicle.Deliver();
+        }
+    }
+
+    public interface IDeliveryVehicle
+    {
+        void Deliver();
+    }
+
+    public class DeliveryBike : IDeliveryVehicle
+    {
+        public void Deliver()
+        {
+            Console.WriteLine("Bike makes a delivery");
+        }
+    }
+   
+    public class DeliveryCar : IDeliveryVehicle
+    {
+        public void Deliver()
+        {
+            Console.WriteLine("Car makes a delivery");
+        }
+    }
+   
+    public class DeliveryVan : IDeliveryVehicle
+    {
+        public void Deliver()
+        {
+            Console.WriteLine("Van makes a delivery");
+        }
+    }
+   
+    public class DeliveryTruck : IDeliveryVehicle
+    {
+        public void Deliver()
+        {
+            Console.WriteLine("Truck makes a delivery");
+        }
+    }
+}
